dcfcf8bd4fa4c26b607aa2f4634a900befc18b38
momingse <da1710080850@gmail.com>
2023-04-12 12:55:39 +0800

    Add hover effect and delete button

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
+import ClearIcon from "@mui/icons-material/Clear";
+
 const Container = styled.div`
   border: 1px solid lightgrey;
   padding: 8px;
   margin-bottom: 8px;
   border-radius: 10px;
-  background-color: ${(props) =>
-    props.isDragDisabled
-      ? "lightgrey"
-      : props.isDragging
-      ? "#E3FCEF"
-      : "white"};
+  background-color: ${(props) => {
+    if (props.isDragDisabled) {
+      return "lightgrey";
+    }
+    if (props.isDragging) {
+      return "#F5F5F5";
+    }
+    return "white";
+  }};
+  &:hover {
+    background-color: #f5f5f5;
+  }
 
   display: flex;
 `;
@@ -24,9 +32,15 @@ const Handle = styled.div`
   margin-right: 8px;
 `;
 
+const ButtonContainer = styled.div`
+  margin-left: auto;
+  display: flex;
+`;
+
 const Task = (props) => {
   //   const [isDragDisabled, setIsDragDisabled] = React.useState(props.task.id === "task-1");
   const [isDragDisabled, setIsDragDisabled] = React.useState(false);
+  const [isShown, setIsShown] = React.useState(false);
 
   return (
     <Draggable
@@ -42,9 +56,19 @@ const Task = (props) => {
           ref={provided.innerRef}
           isDragging={snapshot.isDragging}
           isDragDisabled={isDragDisabled}
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}
         >
           {/* <Handle {...provided.dragHandleProps}/> */}
           {props.task.content}
+          {isShown && (
+            <ButtonContainer>
+              <ClearIcon
+                onClick={e => console.log("delete")}
+                sx={{ fontSize: "15px", color: "grey" }}
+              />
+            </ButtonContainer>
+          )}
         </Container>
       )}
     </Draggable>
